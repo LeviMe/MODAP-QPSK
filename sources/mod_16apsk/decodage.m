34bcd3a2ea074de8d3bc73468f89667fc28950c2
LeviMe <34280110+LeviMe@users.noreply.github.com>
2018-05-28 12:10:52 +0200

    push2

--- sources/mod_16apsk/decodage.m
+++ sources/mod_16apsk/decodage.m
@@ -1,8 +1,10 @@
 function msgDecode = decodage(Rate, Iteration, Signal_Demod)
 
 
+
+bits_decode = reshape(Signal_Demod',[],1);
 H = dvbs2ldpc(Rate);
 dec = comm.LDPCDecoder(H, 'FinalParityChecksOutputPort', true, 'MaximumIterationCount', Iteration, ...
                        'DecisionMethod','Hard decision', 'OutputValue','Information part') ;
-msgDecode = dec(Signal_Demod);
+msgDecode = dec(bits_decode);
                    
---
+++ sources/mod_16apsk/script_test_code.m
@@ -0,0 +1,9 @@
+N = 64800;
+R= 2/3 ; 
+bits=randi([0 6], 9, 1);
+
+
+
+bits_codes = reshape(bits,[],3);
+
+bits_decode = reshape(bits_codes',[],1);
